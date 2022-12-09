09a641bee550507880bfc01ec1a31d18b9554efc
Murtaza Gulamali <murtaza@gulamali.net>
2022-12-09 10:45:49 +0000

    Add solutions for day 8, 2022

--- src/2022/main.go
+++ src/2022/main.go
@@ -4,10 +4,12 @@ import (
 	"gulamali.net/src/2022/01"
 	"gulamali.net/src/2022/05"
 	"gulamali.net/src/2022/06"
+	"gulamali.net/src/2022/08"
 )
 
 func main() {
 	day01.Main()
 	day05.Main()
 	day06.Main()
+	day08.Main()
 }
