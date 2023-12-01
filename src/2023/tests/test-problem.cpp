71c809c008c9da5cc3bfd6d099b9150734bf7994
Murtaza Gulamali <murtaza@gulamali.net>
2023-12-01 12:15:23 +0000

    Refactor tests for Problem class

--- src/2023/tests/test-problem.cpp
+++ src/2023/tests/test-problem.cpp
@@ -11,14 +11,14 @@ TEST_CASE("Problem", "[problem]") {
     };
 
     SECTION("Problem object can be created from filename") {
-        myg::Problem p = myg::Problem(test_filename);
+        myg::Problem p(test_filename);
 
         REQUIRE( p.filename() == test_filename );
         REQUIRE( p.data() == test_data );
     }
 
     SECTION("Problem object can be created from data") {
-        myg::Problem p = myg::Problem(test_data);
+        myg::Problem p(test_data);
 
         REQUIRE( p.filename() == "" );
         REQUIRE( p.data() == test_data );
